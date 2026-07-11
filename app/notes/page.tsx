72dc1584528ace0dbd4cd2a2a8ca398d60611d89
Tetiana <bangchanka0310@gmail.com>
2026-07-11 21:53:14 +0300

    continue with API in Client Side Rendering

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,12 +1,24 @@
-import NoteList from "@/components/NoteList/NoteList";
-import { getNotes } from "@/lib/api";
+'use client';
 
-const Notes = async () => {
-    const response = await getNotes();
+import NoteList from "@/components/NoteList/NoteList";
+import { getNotes, Note } from "@/lib/api";
+import { useState } from "react";
+
+const Notes =  () => {
+    const [notes, setNotes] = useState<Note[]>([]);
+
+    const handleClick = async () => {
+        const response = await getNotes();
+        
+        if (response?.notes) {
+            setNotes(response.notes);
+        }
+    }
 
     return <section>
         <h1>Notes List</h1>
-        {response?.notes?.length > 0 && <NoteList notes={response.notes}/>}
+        <button onClick={handleClick}>Get my notes</button>
+        { notes.length > 0 && <NoteList notes={notes}/>}
     </section>
 }
 
